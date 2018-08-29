60ea5c8393ab385bba78baf272983cded018f3c6
aenirafika <36910850+aenirafika@users.noreply.github.com>
2018-08-29 15:23:53 +0700

    Add files via upload

---
+++ Praktikum-1/Tugas1-java/DataKTP.java
@@ -0,0 +1,20 @@
+public class DataKTP{
+    public static void main(String[] args){
+        System.out.println("\t\t\t\tPROVINSI JAWA TENGAH");
+        System.out.println("\t\t\t\tKABUPATEN PEKALONGAN");
+        System.out.println("\nNIK\t\t\t\t: 3326115112980003");
+        System.out.println("Nama\t\t\t\t: AENI RAFIKA");
+        System.out.println("Tempat/Tgl Lahir\t\t: PEKALONGAN, 11-12-1998");
+        System.out.println("Jenis Kelamin\t\t\t: PEREMPUAN");
+        System.out.println("Gol.Darah\t\t\t: -");
+        System.out.println("Alamat\t\t\t\t: DUKUH KEMASAN");
+        System.out.println("\tRT/RW\t\t\t: 006/003");
+        System.out.println("\tKel/Desa\t\t: KEMASAN");
+        System.out.println("\tKecamatan\t\t: BOJONG");
+        System.out.println("Agama\t\t\t\t: ISLAM");
+        System.out.println("Status Perkawinan\t\t: BELUM KAWIN");
+        System.out.println("Pekerjaan\t\t\t: PELAJAR/MAHASISWA");
+        System.out.println("Kewarganegaraan\t\t\t: WNI");
+        System.out.println("Berlaku Hingga\t\t\t: SEUMUR HIDUP");
+    }
+}
---
+++ Praktikum-1/Tugas1-java/ProfilMhs.java
@@ -0,0 +1,11 @@
+public class ProfilMhs{
+    public static void main(String[] args){
+        System.out.println("\t\t\tIDENTITAS MAHASISWA");
+        System.out.println("\nNIM\t\t\t: 171530004");
+        System.out.println("NAMA\t\t\t: AENI RAFIKA");
+        System.out.println("PRODI\t\t\t: TEKNIK INFORMATIKA");
+        System.out.println("TTL\t\t\t: PEKALONGAN, 11 Desember 1998");
+        System.out.println("AGAMA\t\t\t: ISLAM");
+        System.out.println("ALAMAT\t\t\t: KEMASAN BOJONG");
+    }
+}
